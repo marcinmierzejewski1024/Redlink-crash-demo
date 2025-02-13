8c21d8587043f96ecf2b4389ead955a3c8c7d72f
Marcin Mierzejewski <marcin.mierzejewski@silkycoders.com>
2025-02-13 14:03:24 +0100

    exap,ple app

---
+++ Example123/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example123
+//
+//  Created by Marcin Mierzejewski on 13/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Example123Tests/Example123Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Example123Tests.swift
+//  Example123Tests
+//
+//  Created by Marcin Mierzejewski on 13/02/2025.
+//
+
+import Testing
+@testable import Example123
+
+struct Example123Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
